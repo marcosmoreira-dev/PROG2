085e515bd2aa776f0dcfd1582dc344f4798d53ce
Marcos Vinícius Moreira da Silva <marcos.dev26@gmail.com>
2026-09-02 00:27:32 +0000

    coloca ex4

---
+++ P1/L03/ex04.c
@@ -0,0 +1,16 @@
+/* 4. Fazer uma função denominada divs() que:
+a. recebe como parâmetro um número inteiro n por valor e dois números inteiros max e min por
+referência;
+b. retorna 0 se o número num é primo e 1 caso contrário. Se o número não for primo, as variáveis
+max e min devem assumir os valores do menor e do maior divisores inteiros do número,
+respectivamente, desconsiderando o número 1 e o próprio número num. */
+
+int divs(int n, int *max, int *min);
+
+int main() {
+    return 0;
+}
+
+int divs(int n, int *max, int *min) {
+    
+}
